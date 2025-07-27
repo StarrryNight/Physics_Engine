a61eac7c87bd90a077da80ff337ed018c55666b3
Samuel Lau <lausingsamuel@gmail.com>
2025-07-27 15:20:35 -0700

    remove unused libraries

--- include/Object.h
+++ include/Object.h
@@ -16,7 +16,7 @@ class Object{
         int id,
         glm::vec3 init_pos = glm::vec3(0.0f, 0.0f, 0.0f),
         glm::vec3 init_vel = glm::vec3(0.0f, 0.0f, 0.0f),
-        glm::vec3 init_acc = glm::vec3(0.0f, 981.0f, 0.0f)
+        glm::vec3 init_acc = glm::vec3(0.0f, 1500.0f, 0.0f)
         );
 
 
