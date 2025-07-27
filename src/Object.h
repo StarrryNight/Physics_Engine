b7a111654f5e667e7ec58d65a4b25d155d43fc50
Samuel Lau <lausingsamuel@gmail.com>
2025-07-26 22:19:55 -0700

    add header file for object class

---
+++ src/Object.h
@@ -0,0 +1,45 @@
+//object class
+
+
+#ifndef OBJECT_H
+#define OBJECT_H
+
+#include <GLFW/glfw3.h>
+#include <iostream>
+#include <vector>
+#include <cmath>
+
+class Object{
+    public:  
+        Object(const std::string& name,
+        int id ,
+        float x = 0,
+        float y = 0,
+        float a_ix = 0,
+        float a_iy = 9.81,
+        float v_ix = 0,
+        float v_iy = 0 );
+
+
+        void disrupt(
+        float dx = 0,
+        float dy = 0,
+        float dvx = 0,
+        float dvy = 0,
+        float dax = 0,
+        float day = 0);
+
+        void update(
+            float dt;
+        );
+
+        private:
+            const std::string& name,
+            int id ,
+            float pos_x = 0;
+            float pos_y = 0;
+            float vel_x = 0;
+            float vel_y = 0;
+            float acc_x = 0;
+            float acc_y = 0;
+}
